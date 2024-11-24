dd2a3ec27a9a1f02c317699aea06c3653452d778
manhnguyenrynaster01 <manh.nguyenrynaster01@hcmut.edu.vn>
2024-11-24 23:32:36 +0700

    Init for project stm32

---
+++ souce_code/STM32_project/Core/Inc/button.h
@@ -0,0 +1,30 @@
+/*
+ * button.h
+ *
+ *  Created on: Nov 20, 2024
+ *      Author: ADMIN PC
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+#include <fsm_processing.h>
+
+//THIẾT LẬP TRẠNG THÁI NHẤN
+#define NORMAL_STATE GPIO_PIN_SET
+#define PRESSED_STATE GPIO_PIN_RESET
+#define LONG_PRESS 200
+
+//SỐ LƯỢNG NÚT VÀ NHÃN CÁC NÚT
+#define NUM_OF_BUTTON 3
+#define Select 0
+#define Duration 1
+#define Set 2
+
+void initialButton();
+int isSelectPressed();
+int isDurationPressed();
+int isSetPressed();
+void getKeyInput();
+
+#endif /* INC_BUTTON_H_ */
---
+++ souce_code/STM32_project/Core/Inc/led.h
@@ -0,0 +1,35 @@
+/*
+ * hardware.h
+ *
+ *  Created on: Nov 20, 2024
+ *      Author: ADMIN PC
+ */
+
+#ifndef INC_LED_H_
+#define INC_LED_H_
+
+#include <fsm_processing.h>
+
+#define GREEN0	GPIO_PIN_1
+#define AMBER0	GPIO_PIN_2
+#define RED0	GPIO_PIN_3
+#define GREEN1	GPIO_PIN_4
+#define AMBER1	GPIO_PIN_5
+#define RED1	GPIO_PIN_6
+
+//hiển thị led 7 đoạn
+void display7SEG(int num);
+void update7SEG();
+
+extern int valueSEG12;//Biến lưu giá trị hiển thị led 7 đoạn ở led 1 và 2
+extern int valueSEG34;//Biến lưu giá trị hiển thị led 7 đoạn ở led 3 và 4
+
+//bật tắt đèn giao thông
+void led_on(int pin);
+void led_off(int pin);
+
+//nhấp nháy đèn giao thông
+void blink_red();
+void blink_amber();
+void blink_green();
+#endif /* INC_LED_H_ */
